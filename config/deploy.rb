0c6a985a72a961b8b9912180b8fa37b556401154
Erik <menduina.006@gmail.com>
2014-07-03 23:10:07 -0300

    deployment configs

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,34 +1,9 @@
 require "bundler/capistrano"
-require "rvm/capistrano"
 
 server "104.131.254.89", :web, :app, :db, primary: true
 
-# set :scm, :git # You can set :scm explicitly or Capistrano will make an intelligent guess based on known version control directory names
-# Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
-
-# role :web, "your web-server here"                          # Your HTTP server, Apache/etc
-# role :app, "your app-server here"                          # This may be the same as your `Web` server
-# role :db,  "your primary db-server here", :primary => true # This is where Rails migrations will run
-# role :db,  "your slave db-server here"
-
-# if you want to clean up old releases on each deploy uncomment this:
-# after "deploy:restart", "deploy:cleanup"
-
-# if you're still using the script/reaper helper you will need
-# these http://github.com/rails/irs_process_scripts
-
-# If you are using Passenger mod_rails uncomment this:
-# namespace :deploy do
-#   task :start do ; end
-#   task :stop do ; end
-#   task :restart, :roles => :app, :except => { :no_release => true } do
-#     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
-#   end
-# end
-
 set :application, "projetomatematica"
-set :user, "projetomatematica"
-set :port, 6012
+set :user, "admin"
 set :deploy_to, "/home/#{user}/apps/#{application}"
 set :deploy_via, :remote_cache
 set :use_sudo, false
@@ -36,7 +11,6 @@ set :use_sudo, false
 set :scm, "git"
 set :repository, "git@github.com:erikcabral/#{application}.git"
 set :branch, "master"
-
 
 default_run_options[:pty] = true
 ssh_options[:forward_agent] = true
--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,10 +1,10 @@
-root = "/home/projetomatematica/apps/projetomatematica/current"
+root = "/home/admin/apps/projetomatematica/current"
 working_directory root
 pid "#{root}/tmp/pids/unicorn.pid"
 stderr_path "#{root}/log/unicorn.log"
 stdout_path "#{root}/log/unicorn.log"
 
-listen "/tmp/unicorn.projectname.sock"
+listen "/tmp/unicorn.projetomatematica.sock"
 worker_processes 2
 timeout 30
 
