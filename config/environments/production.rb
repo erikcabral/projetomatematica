17eebafcdf85fae05a4a39a48cf7649424b64e2b
Erik <menduina.006@gmail.com>
2014-07-12 20:27:06 -0300

    Agora email é enviado após submit no form

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -89,11 +89,12 @@ Papirapp::Application.configure do
   config.action_mailer.delivery_method = :smtp
 
   config.action_mailer.smtp_settings = {
-  address: "smtp.mandrillapp.com",
-  port: 587,
-  domain: "projetomatematica.com.br",
-  authentication: "plain",
-  enable_starttls_auto: true,
-  user_name: ENV["MAIL_USERNAME"],
-  password: ENV["MANDRILL_APIKEY"]
+    address: "smtp.mandrillapp.com",
+    port: 587,
+    domain: "projetomatematica.com.br",
+    authentication: "plain",
+    enable_starttls_auto: true,
+    user_name: ENV["MAIL_USERNAME"],
+    password: ENV["MANDRILL_APIKEY"]
+  }
 end
